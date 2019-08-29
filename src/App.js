aae914bc4caf6d0ba3afa60f5ba27ec087e181c9
Taylor Tobin <19tt94@gmail.com>
2019-08-29 14:19:36 -0700

    grid setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
 import React from 'react';
-import Grid from './components/grid/Grid.js';
 import './App.scss';
+import Grid from './components/grid/Grid.js';
+import Form from './components/form/Form.js';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
+    <AppWrapper>
       <header className="App-header">
-        <span>B.H. Custom Logo</span>
-        <div className="background"></div>
-        <div className="bar">
-            <h1 className="header">B.H. Custom</h1>
-        </div>
+        <Image src={require('./assets/images/logo.png')} />
+        <Banner />
+        <Bike src={require('./assets/images/bike.png')} />
+        <Bar>
+            <Header>S.L.O. Based Building Solutions <span>by Brian Hamm</span></Header>
+            <Description>
+              Lorem ipsum dolor amet tote bag banjo vegan yuccie iPhone +1 vinyl sriracha. Taxidermy mlkshk whatever fashion axe deep v meditation plaid. Beard trust fund paleo ugh irony. Kickstarter slow-carb +1, tilde cold-pressed post-ironic drinking vinegar iceland.
+            </Description>
+        </Bar>
       </header>
 
-      <p className="description">
-        Lorem ipsum dolor amet tote bag banjo vegan yuccie iPhone +1 vinyl sriracha. Taxidermy mlkshk whatever fashion axe deep v meditation plaid. Beard trust fund paleo ugh irony. Kickstarter slow-carb +1, tilde cold-pressed post-ironic drinking vinegar iceland.
-      </p>
-
       <Grid />
-    </div>
+
+      <Form />
+
+      <Footer>
+        &copy; Brian Hamm 2019
+      </Footer>
+
+    </AppWrapper>
   );
 }
+
+const AppWrapper = styled.div`
+    min-height: 100vh;
+`;
+
+const Image = styled.img`
+    margin-top: 50px;
+    width: 30%;
+`;
+
+const Banner = styled.div`
+    width: 100%;
+    height: 100px;
+    position: relative;
+    padding-top: $pad*3;
+    overflow: hidden;
+
+    &:before {
+        content: '';
+        position: absolute;
+        top: -10px;
+        left: 40px;
+        width: 300px;
+        height: 300px;
+        background: #A85751;
+        border-radius: 50%;
+        margin: 10px;
+        box-shadow: 10px 8px 10px rgba(0,0,0,0.5);
+        z-index: 2;
+    }
+
+    &:after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 25px;
+        background: #553739;
+        margin: 10px 0;
+        box-shadow: 0 0 15px rgba(0,0,0,0.5);
+        z-index: 3;
+        overflow: hidden;
+    }
+`;
+
+const Bike = styled.img`
+    width: 25%;
+    position: absolute;
+    top: 20%;
+    right: 0;
+    z-index: 4;
+`;
+
+const Bar = styled.div`
+    background: #232020;
+    width: 100%;
+    box-shadow: 0 0 15px rgba(0,0,0,0.8);
+`;
+
+const Header = styled.h1`
+    font-size: 35px;
+    padding: 10px;
+    margin: 40px auto 5px;
+    max-width: 900px;
+    color: #fff;
+
+    span {
+        font-size: 12px;
+    }
+`;
+
+const Description = styled.p`
+    font-size: 14px;
+    line-height: 1.5;
+    color: #fff;
+    position: relative;
+    width: 100%;
+    padding: 10px;
+    margin: 5px auto 40px;
+    max-width: 900px;
+`;
+
+const Footer = styled.div`
+    background: #232020;
+    width: 100%;
+    padding: 20px;
+    color: #fff;
+`;
 
 export default App;
--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,118 +1,151 @@
 import React from 'react';
 import './Grid.scss';
+import styled from 'styled-components';
+
+// IMAGES
+import Texture from '../../assets/images/texture.png';
 
 class Grid extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            // ADD YOUR IG ACCESS TOKEN HERE (ig.token) CAN EASILY GET USING ~ http://instagram.pixelunion.net/
+            access_token: "14441723451.1677ed0.e649f4f05271442b8b68fe27ca986a34",
+            user_id: "14441723451",
+            num_posts: "6",
+            row_length: "4",
+            instagram: null,
+            loaded: false
+        };
+        // Data Binding
+        this.jsInstagram = this.jsInstagram.bind(this);
+        this.getJSONP = this.getJSONP.bind(this);
+    }
+
     render() {
         return (
-            <section className="grid">
+            <GridWrapper data-userid={this.state.user_id} data-limit={this.state.num_posts}>
+
                 <div id="instagram" data-userid="" data-limit=""></div>
-                <button onClick={activateLasers}>Load More</button>
-                // <a href="#!" className="instagram__loadButton js-instagram__loadButton">Load More</a>
-            </section>
+                <button>Load More</button>
+            </GridWrapper>
         );
     }
 
     componentDidMount() {
         // ADD YOUR IG ACCESS TOKEN HERE (ig.token) CAN EASILY GET USING ~ http://instagram.pixelunion.net/
-        let accessToken = "408299344.1677ed0.5a73bb001edb42eaa3eda42979b088e6";
+        let accessToken = "14441723451.1677ed0.e649f4f05271442b8b68fe27ca986a34";
         // let accessToken = "";
         // ADD YOUR USER ID HERE ~ THIS IS THE FIRST SECTION OF THE IG TOKEN EG. 1234567890
-        let userID = "408299344";
+        let userID = "14441723451";
         // THE AMMOUNT OF INSTAGRAM IMAGES YOU WANT TO DISPLAY
-        let numPosts = "8"
+        let numPosts = "6"
         // THE AMMOUNT OF INSTAGRAM IMAGES YOU WANT TO ADD WITH EACH CLICK
         let rowLength = "4"
 
-        jsInstagram(accessToken, userID, numPosts, rowLength);
+        this.jsInstagram(accessToken, userID, numPosts, rowLength);
+        console.log(this.state.instagram);
     }
-}
 
-function jsInstagram(access_token, user_id, posts, rows) {
-    let instagram = document.getElementById('instagram');
-    if(access_token === '') {
-        let placeholder = "<a class='instagram__placeholder'></a>".repeat(posts);
-        instagram.insertAdjacentHTML('beforeend', placeholder);
-    } else {
-        instagram.setAttribute('data-userid', user_id);
-        instagram.setAttribute('data-limit', posts);
+    jsInstagram(access_token, user_id, posts, rows) {
+        let instagram = document.getElementById('instagram');
+        if(access_token === '') {
+            let placeholder = "<a class='instagram__placeholder'></a>".repeat(posts);
+            instagram.insertAdjacentHTML('beforeend', placeholder);
+        } else {
+            instagram.setAttribute('data-userid', user_id);
+            instagram.setAttribute('data-limit', posts);
 
-        let ig = {};
-        ig.token = access_token;
+            let ig = {};
+            ig.token = access_token;
 
-        ig.init = ()=> {
-            let args = {};
-            args.container = instagram;
-            args.userid = args.container.getAttribute('data-userid');
-            args.limit = args.container.getAttribute('data-limit');
-            args.feedurl = 'https://api.instagram.com/v1/users/' + args.userid + '/media/recent/?access_token=' + ig.token + '&count=' + args.limit + '&callback=?';
-            args.html = '';
-            // PASS ARGS TO QUERY
-            ig.query(args);
+            ig.init = ()=> {
+                let args = {};
+                args.container = instagram;
+                args.userid = args.container.getAttribute('data-userid');
+                args.limit = args.container.getAttribute('data-limit');
+                args.feedurl = 'https://api.instagram.com/v1/users/' + args.userid + '/media/recent/?access_token=' + ig.token + '&count=' + args.limit + '&callback=?';
+                args.html = '';
+                // PASS ARGS TO QUERY
+                ig.query(args);
+            }
+
+        ig.query = (args)=> {
+            this.getJSONP(args.feedurl, (data)=> {
+                this.setState({
+                    instagram: data
+                });
+                console.log(this.state);
+
+                // PASS QUERY DATA TO BUILDER
+                ig.build(data, args);
+            });
         }
 
-    ig.query = (args)=> {
-        getJSONP(args.feedurl, (data)=> {
-            // PASS QUERY DATA TO BUILDER
-            ig.build(data, args);
-        });
-    }
+        ig.build = (data, args)=> {
+            console.log(data.data);
+            data.data.forEach((item, i)=> {
+                let link = item.link || false;
+                let likes = item.likes.count || false;
+                let thumb = item.images.low_resolution.url;
+                let img = item.images.standard_resolution.url;
+                // get 1280 size photo [hack until avail in api]
+                let hires = img.replace('s640x640', '1080x1080');
+                args.html += '<a href="' + link + '" class="image" target="_blank" style="background-image: url(' + img + ');" data-img="' + hires + '">';
+                if (likes) {
+                    args.html += '<span class="likes">'+likes+'</span>';
+                    args.html += '</a>';
+                }
+            })
 
-    ig.build = (data, args)=> {
-        console.log(data.data);
-        data.data.forEach((item, i)=> {
-            let link = item.link || false;
-            let likes = item.likes.count || false;
-            let thumb = item.images.low_resolution.url;
-            let img = item.images.standard_resolution.url;
-            // get 1280 size photo [hack until avail in api]
-            let hires = img.replace('s640x640', '1080x1080');
-            args.html += '<a href="' + link + '" class="image" target="_blank" style="background-image: url(' + img + ');" data-img="' + hires + '">';
-            if (likes) {
-                args.html += '<span class="likes">'+likes+'</span>';
-                args.html += '</a>';
-            }
-        })
+            // PASS TO OUTPUT
+            ig.output(args);
+        }
 
-        // PASS TO OUTPUT
-        ig.output(args);
-    }
+        ig.output = (args)=> {
+            instagram.insertAdjacentHTML('beforeend', args.html);
+        }
 
-    ig.output = (args)=> {
-        instagram.insertAdjacentHTML('beforeend', args.html);
-    }
+        ig.view = {
+          viewer: document.getElementsByClassName('igviewer'),
+          image: document.getElementsByClassName('igviewer img'),
+          open: function(img) {
+              console.log(ig.view.viewer);
+              ig.view.viewer.removeClass('hidden');
+              ig.view.image.attr('src', img);
+              console.log(ig.view.image);
+          },
+          close: function() {
+              ig.view.viewer.addClass('hidden');
+              ig.view.image.attr('src', '');
+          }
+        }
 
-    ig.view = {
-      viewer: document.getElementsByClassName('igviewer'),
-      image: document.getElementsByClassName('igviewer img'),
-      open: function(img) {
-          console.log(ig.view.viewer);
-          ig.view.viewer.removeClass('hidden');
-          ig.view.image.attr('src', img);
-          console.log(ig.view.image);
-      },
-      close: function() {
-          ig.view.viewer.addClass('hidden');
-          ig.view.image.attr('src', '');
+        ig.init();
       }
     }
 
-    ig.init();
-  }
+    getJSONP(url, success) {
+        let ud = '_' + +new Date,
+            script = document.createElement('script'),
+            head = document.getElementsByTagName('head')[0]
+                   || document.documentElement;
+
+        window[ud] = function(data) {
+            head.removeChild(script);
+            success && success(data);
+        };
+
+        script.src = url.replace('callback=?', 'callback=' + ud);
+        head.appendChild(script);
+    }
 }
 
-function getJSONP(url, success) {
-    let ud = '_' + +new Date,
-        script = document.createElement('script'),
-        head = document.getElementsByTagName('head')[0]
-               || document.documentElement;
-
-    window[ud] = function(data) {
-        head.removeChild(script);
-        success && success(data);
-    };
-
-    script.src = url.replace('callback=?', 'callback=' + ud);
-    head.appendChild(script);
-}
+const GridWrapper = styled.section`
+    padding: 5%;
+    background-image: url(${Texture});
+    background-repeat: repeat;
+`;
 
 export default Grid;
